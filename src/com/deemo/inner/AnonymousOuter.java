64f7481a570ec8dd1adff3ab2d8d1a50a82ab83c
DeemoTlz <tlh520>
2020-05-18 10:16:11 +0800

    完善匿名内部类测试代码

--- src/com/deemo/inner/AnonymousOuter.java
+++ src/com/deemo/inner/AnonymousOuter.java
@@ -6,9 +6,9 @@ public class AnonymousOuter {
 	public int b = 2;
 	private static int c = 1;
 	public static int d = 2;
-	String game = "三男一狗";
+	String gameClass = "三男一狗";
 
-	public void print(final String game, final GTA gta) {
+	public void print(final String gameMethodParam, final GTA gtaMethodParam) {
 		/*new C() {
 			@Override
 			public void shopping() {
@@ -37,11 +37,21 @@ public class AnonymousOuter {
 			}
 		}.game();*/
 
-		final String myGame = "三男一狗";
-		final GTA myGta = new GTA();
+		final String gameMethod = "三男一狗";
+		final GTA gtaMethod = new GTA();
 		String games = "三男一狗";
 
 		new Super() {
+			@Override
+			public void eat() {
+
+			}
+
+			@Override
+			public void thought() {
+
+			}
+
 			@Override
 			public void save() {
 			}
@@ -52,49 +62,41 @@ public class AnonymousOuter {
 			}
 
 			@Override
-			public void game(String games, GTA gta1) {
+			public void playGame(String games, GTA gta) {
+				System.out.println("外部类 调用方法的形参：" + gameMethodParam);
+				System.out.println("外部类 成员变量：" + AnonymousOuter.this.gameClass);
+				System.out.println("外部类 成员变量：" + AnonymousOuter.c);
+				gtaMethodParam.play();
+				gtaMethodParam.setName("给他爱5");
+
+				System.out.println("==========================");
+
+				System.out.println("外部类 方法成员变量：" + gameMethod);
+				System.out.println("外部类 方法成员变量：" + gtaMethod);
+
+				System.out.println("==========================");
+
+				System.out.println("匿名内部类 方法参数：" + games);
 				// 修改形参，对实参没有影响
 				games = "给他爱5";
-				// 通过引用地址修改，会同步修改
-				gta1.setName("GTA 5");
 				// 将形参指向新对象时，会中断对实参的引用
-				// gta1 = new GTA();
-				System.out.println("外部类调用方法形参：" + game);
-				System.out.println("匿名内部类方法参数：" + games);
-				System.out.println("匿名内部类成员变量：" + myGame);
-				System.out.println("匿名内部类成员变量：" + myGta);
-				System.out.println("外部类成员变量：" + AnonymousOuter.this.game);
+				// gta = new GTA();
+				// 通过引用地址修改，会同步修改
+				gta.setName("GTA 5");
 
-				gta.play();
-				gta.setName("给他爱5");
+				System.out.println("==========================");
 			}
+		}.playGame(games, gtaMethod);
 
-			@Override
-			public void work() {
-
-			}
-
-			@Override
-			public void walk() {
-
-			}
-		}.game(games, myGta);
 		System.out.println("修改形参：games 后：" + games);
-		System.out.println("修改形参：myGta 后：" + myGta);
-
-		new D() {
-			@Override
-			void a() {
-
-			}
-		};
+		System.out.println("修改形参：gta 后：" + gtaMethod);
 	}
 
 	public static void main(String[] args) {
-		AnonymousOuter AnonymousOuter = new AnonymousOuter();
+		AnonymousOuter anonymousOuter = new AnonymousOuter();
 
 		GTA gta = new GTA();
-		AnonymousOuter.print("三男一狗", gta);
+		anonymousOuter.print("三男一狗", gta);
 		gta.play();
 	}
 
@@ -110,18 +112,25 @@ class GTA {
 	public void setName(String name) {
 		this.name = name;
 	}
+
+	@Override
+	public String toString() {
+		return "GTA{" +
+				"name='" + name + '\'' +
+				'}';
+	}
 }
 
 interface Animal {
-	void walk();
+	void eat();
 }
 
 interface Person extends Animal {
-	void work();
+	void thought();
 }
 
 interface Man extends Person {
-	void game(String games, GTA gta1);
+	void playGame(String games, GTA gta1);
 }
 
 interface Woman extends Person {
@@ -132,17 +141,17 @@ interface Super extends Man, Woman {
 	void save();
 }
 
-abstract class A {
+/*abstract class A {
 	abstract void a();
 }
 
-/*abstract class B {
+abstract class B {
 	abstract void b();
 }
 
 abstract class C extends A implements Man, Woman {
 
-}*/
+}
 
 class D extends A {
 
@@ -150,5 +159,4 @@ class D extends A {
 	void a() {
 
 	}
-}
-
+}*/
